a0772dd8223d2b095755aa1ed52845ee53ae5a58
gladisor <123.tristan.shah@gmail.com>
2021-06-01 21:07:36 -0400

    minor changes to tristan

--- test.py
+++ test.py
@@ -2,17 +2,18 @@ import requests
 import json
 import datetime
 
-after = str(int(datetime.datetime(2019, 1, 1).timestamp()))
-before = str(int(datetime.datetime(2019, 2, 1).timestamp()))
+after = int(datetime.datetime(2019, 1, 1).timestamp())
+before = int(datetime.datetime(2019, 10, 1).timestamp())
 sub = 'Advice'
 
 print(after)
 print(before)
-
-url = f'https://api.pushshift.io/reddit/search/submission/?after={after}$before={before}&subreddit={sub}&size={100}'
-
+# nlt18t
+# url = f'https://api.pushshift.io/reddit/search/submission/?after={after}$before={before}&subreddit={sub}&size={100}'
+url = f'https://api.pushshift.io/reddit/search/submission/?q=Advice%Recived&after={after}&before={before}&subreddit=Advice&size={1000}'
+# q=flair_name%3A%22Advice%Recived%22&restrict_sr=1
 r = requests.get(url)
-print(r)
 data = json.loads(r.text)
 
-print(data)
+for post in data['data']:
+    print(post['title'])
