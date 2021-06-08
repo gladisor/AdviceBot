d1df8d10c194683c714f79ee61ed132d0b7ab5d4
gladisor <123.tristan.shah@gmail.com>
2021-06-08 00:49:16 -0400

    adding good requirements

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 import datetime
 from utils import get_submissions
+import pandas as pd
 
 after = int(datetime.datetime(2019, 1, 1).timestamp())
 before = int(datetime.datetime(2019, 1, 2).timestamp())
 sub = 'Advice'
-size = 85
-min_comments = 3
 
-submissions = get_submissions(after, before, sub, size, min_comments)
+submissions = get_submissions(after, before, sub)
+#
+# for post in submissions:
+#     print(post['title'], '\n')
+#
+# print(len(submissions))
 
-for post in submissions:
-    print(post['title'], '\n')
+data = pd.DataFrame(submissions)
 
-print(len(submissions))
+print(data)
