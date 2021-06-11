aee40813f3acef256a9208d1cb6b3a7460c66f7f
gladisor <123.tristan.shah@gmail.com>
2021-06-11 12:08:49 -0400

    data mining

--- test.py
+++ test.py
@@ -1,22 +1,29 @@
 import datetime
 from utils import get_submissions, get_comments
-import pandas as pd
 import requests
 import json
 
-after = int(datetime.datetime(2019, 1, 1).timestamp())
-before = int(datetime.datetime(2019, 1, 2).timestamp())
+after = int(datetime.datetime(2018, 1, 3).timestamp())
+before = int(datetime.datetime(2018, 1, 4).timestamp())
 sub = 'Advice'
 size = 5
 
 submissions = get_submissions(after, before, sub, size)
 
-data = pd.DataFrame(submissions)
-print(data)
+for submission in submissions:
+    f = open(submission['id'] + '.txt', 'w', encoding='utf-8')
+    f.write('Query:\n')
+    f.write(submission['selftext'])
+    f.write('\n\n')
 
-post_id = data['id'][0]
-print(post_id)
+    comments = get_comments(submission['id'])
 
-comments = get_comments(post_id)
+    if len(comments) > 0:
+        comments = filter(lambda x: x['link_id'] == x['parent_id'], comments)
 
-print(len(comments))
+        for comment in comments:
+            print(comment['body'])
+            f.write('Response:\n')
+            f.write(comment['body'])
+            f.write('\n\n')
+    f.close()
