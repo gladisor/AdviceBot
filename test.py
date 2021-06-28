239a31f92cd3b110b450c0e094c5bd9ff2f5cbea
gladisor <123.tristan.shah@gmail.com>
2021-06-28 19:54:45 -0400

    added minor changes

--- test.py
+++ test.py
@@ -7,8 +7,14 @@ day = 1
 
 after = int(datetime.datetime(2018, 1, day).timestamp())
 before = int(datetime.datetime(2018, 1, day+1).timestamp())
-sub = 'Advice'
+new = datetime.datetime.fromtimestamp(after) + datetime.timedelta(days=1)
 
-submissions = get_submissions(after, before, sub, size=5)
+print(f'after {after}')
+print(f'before {before}')
+print(f'new {int(new.timestamp())}')
 
-get_data(submissions)
+# sub = 'Advice'
+#
+# submissions = get_submissions(after, before, sub, size=5)
+#
+# get_data(submissions)
